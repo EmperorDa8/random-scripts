62d94847b58ac7b3bad0d6c4f99daf0c13bf2003
EmperorDae <uabdul88@gmail.com>
2022-01-14 12:53:10 -0800

    add new script

---
+++ uppered.py
@@ -0,0 +1,8 @@
+def highlight_word(sentence, word):
+	return(word in sentence.replace(word,word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
+
+	       
