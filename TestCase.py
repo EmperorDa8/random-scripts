2ae03b33ccc9b4a5e21e13e4b83a83e05bdde7d0
EmperorDae <uabdul88@gmail.com>
2022-01-14 11:30:34 -0800

    adding new test scripts to git repo

---
+++ TestCase.py
@@ -0,0 +1,35 @@
+import requests,json
+import jsonpath
+
+
+url="https://reqres.in/api/users?page=2"
+
+def test_user():
+    #read the file with file handling module
+    with open ("C:\\Users\\USER\\post user api.json","r")as fil:
+        jsonfil=fil.read()
+        json_req=json.loads(jsonfil)   
+    # make put requests json object
+    res=requests.put(url,json_req)
+    #validate post response code, if ans is blank , successful
+    assert res.status_code==200
+    #fetch header from requests or to find specific header content use ,get
+    #print(res.headers.get("Date"))
+    #print(res.headers)
+    new_json=json.loads(res.text)
+    sub=jsonpath.jsonpath(new_json,'name')
+    print(sub[0])
+    
+    
+def test_user2():
+    #read the file with file handling module
+    with open ("C:\\Users\\USER\\post user api.json","r")as fil:
+        jsonfil=fil.read()
+        json_req=json.loads(jsonfil)   
+    # make put requests json object
+    res=requests.put(url,json_req)
+    #validate post response code, if ans is blank , successful
+    assert res.status_code==203
+    new_json=json.loads(res.text)
+    sub=jsonpath.jsonpath(new_json,'id')
+    print(sub[0])
