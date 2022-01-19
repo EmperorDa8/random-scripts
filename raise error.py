03d31fd656c2587d64bc158a5d093e48c0ab7324
EmperorDae <uabdul88@gmail.com>
2022-01-19 13:52:37 -0800

    add few lines and created 3 functions with different purpose

---
+++ raise error.py
@@ -0,0 +1,26 @@
+
+def word(texts):
+    name=input("enter a name: ")
+    if not name.isalpha() :
+    raise NameError("pls enter a character!")
+    rr=(f"{name}@gmail.com")
+    return rr
+
+print(word("isreal"))
+
+#def recharge(network,number):
+    #if len(number) > 11:
+        #raise ValueError("number entered is over the limit!")
+    #final=f"the {network} number is this {number}"
+    #return final
+
+#print(recharge(GLO,"08034061357"))
+
+def grade_score(grade):
+    grade=input("pls enter a grade A or B")
+    if grade == "A":
+        print("90%")
+    elif grade == "B":
+        print("45%")
+
+grade_score("60")
