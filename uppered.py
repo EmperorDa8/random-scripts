791923911551eb133801ff7abc34b934fcb66212
Dan Usman <86113954+EmperorDa8@users.noreply.github.com>
2022-01-23 10:06:23 -0800

    Update uppered.py
    add one line of call to the function

--- uppered.py
+++ uppered.py
@@ -4,5 +4,5 @@ def highlight_word(sentence, word):
 print(highlight_word("Have a nice day", "nice"))
 print(highlight_word("Shhh, don't be so loud!", "loud"))
 print(highlight_word("Automating with Python is fun", "fun"))
-
+print(highlight_word("pythoneer with persistance","persistance")
 	       
