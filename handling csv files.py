ad701412a8a11b664a91a87d87a76408c1b1ce54
EmperorDae <uabdul88@gmail.com>
2022-02-03 05:14:35 -0800

    upload to repo

--- handling csv files.py
+++ handling csv files.py
@@ -10,7 +10,6 @@ def read_employees(csv_file_location):
             employee_list.append(data)
       return (employee_list)
 
-
 def process_data(employee_list):
       department_list = []
       for employee_data in employee_list:
@@ -19,7 +18,6 @@ def process_data(employee_list):
       for department_name in set(department_list):
             department_data[department_name] = department_list.count(department_name)
       return department_data
-
 Employee_list=read_employees("C:\\Users\\USER\\Documents\\1000 Records.csv")
 
 dictionary = process_data(Employee_list)
